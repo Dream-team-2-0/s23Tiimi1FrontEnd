040cc029f8f8e2cf99870ba6607ea0234b824ef2
Aaron Hirsimäki <hirsimakiaaronn@gmail.com>
2023-11-01 14:14:57 +0200

    ekat reactit

---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home() {
+
+    return (
+      <h1>Store Page :D</h1>
+    );
+  }
---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export default function Products() {
+
+    const [products,setProducts] = useEffect([]);
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
