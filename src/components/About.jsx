5fffd32d5821843b4b7ea1881b02dbec36c54ec1
oliviahallenberg1 <bgz829@myy.haaga-helia.fi>
2023-11-06 14:31:22 +0200

    about-sivun keskitys

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,25 @@
 
 export default function About() {
 
-    return (
-        <div style={{ paddingLeft: '15%', paddingRight: '15%'}}>
-            <h1>About us </h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Commodo nulla facilisi nullam vehicula ipsum a. Egestas dui id ornare arcu. Non sodales neque sodales ut etiam.
-                Nisl tincidunt eget nullam non nisi. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.
-                Facilisi cras fermentum odio eu feugiat. Nisi est sit amet facilisis magna etiam. At risus viverra adipiscing at in.
-                Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Gravida cum sociis natoque penatibus et magnis dis parturient montes.
-                Tellus mauris a diam maecenas sed enim.
-            </p>
-            <p>
-                In fermentum et sollicitudin ac. Dictum non consectetur a erat nam at lectus urna duis.
-                Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
-                Sapien eget mi proin sed libero. Facilisis gravida neque convallis a. Cursus sit amet dictum sit amet justo donec. Risus sed vulputate odio ut.
-                Netus et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam. Ornare lectus sit amet est placerat in. Varius duis at consectetur lorem donec.
-                Eu consequat ac felis donec.
-            </p>
+  return (
+    <div style={{ paddingLeft: '15%', paddingRight: '15%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', flexDirection: 'column' }}>
+      <h1>About us </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Commodo nulla facilisi nullam vehicula ipsum a. Egestas dui id ornare arcu. Non sodales neque sodales ut etiam.
+        Nisl tincidunt eget nullam non nisi. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.
+        Facilisi cras fermentum odio eu feugiat. Nisi est sit amet facilisis magna etiam. At risus viverra adipiscing at in.
+        Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Gravida cum sociis natoque penatibus et magnis dis parturient montes.
+        Tellus mauris a diam maecenas sed enim.
+      </p>
+      <p>
+        In fermentum et sollicitudin ac. Dictum non consectetur a erat nam at lectus urna duis.
+        Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
+        Sapien eget mi proin sed libero. Facilisis gravida neque convallis a. Cursus sit amet dictum sit amet justo donec. Risus sed vulputate odio ut.
+        Netus et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam. Ornare lectus sit amet est placerat in. Varius duis at consectetur lorem donec.
+        Eu consequat ac felis donec.
+      </p>
 
-        </div>
-    );
+    </div>
+  );
 }
