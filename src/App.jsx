c95ed86d91840c483b4e3b1ec7b729bb3e785742
oliviahallenberg1 <bgz829@myy.haaga-helia.fi>
2023-11-08 11:54:23 +0200

    poistettu home-page ja lisätty tyyliä

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@
 //import './App.css'
 import Products from './components/Products'
 import About from './components/About'
-import Home from './components/Home';
 import { useState } from 'react';
 import { Tab, Tabs } from '@mui/material';
 
 
 function App() {
 
-  const [value, setValue] = useState('Home');
+  const [value, setValue] = useState('');
 
   const handleChange = (_, value) => {
     setValue(value);
@@ -19,15 +18,14 @@ function App() {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
       <h1>💛 🐶 Passion for Dog Fashion 🐶 💛</h1>
       <Tabs
-        TabIndicatorProps={{ style: { background: 'black' } }}
+        TabIndicatorProps={{ style: { background: 'black' }, text: 'black' }}
         value={value}
         onChange={handleChange}
         centered={true}
-        color="pink">
-        <Tab
-          value="Home"
-          label="Home">
-        </Tab>
+        color="pink"
+        style={{ backgroundColor: 'lightyellow' }}
+        textColor="inherit"
+        indicatorColor="black">
         <Tab
           value="Products"
           label="Products">
@@ -37,7 +35,6 @@ function App() {
           label="About">
         </Tab>
       </Tabs>
-      {value === "Home" && <Home />}
       {value === "Products" && <Products />}
       {value === "About" && <About />}
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,30 @@ export default function About() {
   return (
     <div style={{ paddingLeft: '15%', paddingRight: '15%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', flexDirection: 'column' }}>
       <h1>About us </h1>
+    
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Commodo nulla facilisi nullam vehicula ipsum a. Egestas dui id ornare arcu. Non sodales neque sodales ut etiam.
-        Nisl tincidunt eget nullam non nisi. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue.
-        Facilisi cras fermentum odio eu feugiat. Nisi est sit amet facilisis magna etiam. At risus viverra adipiscing at in.
-        Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Gravida cum sociis natoque penatibus et magnis dis parturient montes.
-        Tellus mauris a diam maecenas sed enim.
+        Passion for Dog Fashion on vuonna 2023 perustettu koiranvaateyritys. Missionamme on tuoda kaikki tyylikkäimmät koiran vaatteet,
+        asusteet ja tarvikkeet helposti asiakkaan saataville yhteen paikkaan.
       </p>
+      <p> Y-Tunnus: 1122334-1</p>
+      <table>
+        <tr>
+          <th>Omistajat: </th>
+        </tr>
+        <tr>
+          <td>
+            M. Aaltonen </td>
+          <td>  O. Hallenberg </td>
+          <td>  A. Hirsimäki </td>
+          <td>  E. Hokkanen </td>
+          <td>  A. Hyppönen </td>
+        </tr>
+      </table>
+      <br />
       <p>
-        In fermentum et sollicitudin ac. Dictum non consectetur a erat nam at lectus urna duis.
-        Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
-        Sapien eget mi proin sed libero. Facilisis gravida neque convallis a. Cursus sit amet dictum sit amet justo donec. Risus sed vulputate odio ut.
-        Netus et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam. Ornare lectus sit amet est placerat in. Varius duis at consectetur lorem donec.
-        Eu consequat ac felis donec.
+        Ota yhteyttä:
+        Ratapihantie 13, 00520 Helsinki <br />
+        ceo@passionfordogfashion.fi
       </p>
 
     </div>
