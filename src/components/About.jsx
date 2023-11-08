c0a04f4eaba0b077a604cc1069a6edcee1069b33
oliviahallenberg1 <bgz829@myy.haaga-helia.fi>
2023-11-08 12:12:52 +0200

    lisätty lore

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,27 @@ export default function About() {
   return (
     <div style={{ paddingLeft: '15%', paddingRight: '15%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', flexDirection: 'column' }}>
       <h1>About us </h1>
-    
+
       <p>
         Passion for Dog Fashion on vuonna 2023 perustettu koiranvaateyritys. Missionamme on tuoda kaikki tyylikkäimmät koiran vaatteet,
         asusteet ja tarvikkeet helposti asiakkaan saataville yhteen paikkaan.
+        Haluamme varmistaa, että jokainen karvainen ystävämme voi tuntea olonsa mukavaksi näyttäen samalla upealta.
+        Passion forDog Fashionin valikoima koostuu monipuolisesta tarjonnasta koiranvaatteita, kuten takkeja, sukkia, hattuja ja koiranpetejä,
+        jotka on valikoitu verkkokauppaamme sekä koiran että omistajan tyylikkyyttä ja trendikkyyttä silmällä pitäen.
+        Vaatteemme on valmistettu laadukkaista ja kestävistä materiaaleista, jotka ovat myös mukavia koiran päällä.
+        Ne suojaavat koiraa sään vaihteluilta, tarjoavat lämpöä kylminä päivinä ja lisäävät samalla koirasi yleistä viehätysvoimaa. Myös muut
+        tuotteemme on tehty vastuullisista raaka-aineista laatua ja trendikkyyttä unohtamatta. Olemme sitoutuneet tarjoamaan asiakkaillemme eettisiä
+        ja vastuullisia vaihtoehtoja koiransa vaatetuksen suhteen.
       </p>
       <p> Y-Tunnus: 1122334-1</p>
       <table>
         <tr>
+          <th></th>
+          <th></th>
           <th>Omistajat: </th>
         </tr>
         <tr>
-          <td>
-            M. Aaltonen </td>
+          <td> M. Aaltonen </td>
           <td>  O. Hallenberg </td>
           <td>  A. Hirsimäki </td>
           <td>  E. Hokkanen </td>
@@ -26,6 +34,7 @@ export default function About() {
       <br />
       <p>
         Ota yhteyttä:
+        <br />
         Ratapihantie 13, 00520 Helsinki <br />
         ceo@passionfordogfashion.fi
       </p>
